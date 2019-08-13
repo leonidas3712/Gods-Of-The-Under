74c534aec39d7e6957331d43dcef3dc21b0ec0cd
omri <42713743+leonidas3712@users.noreply.github.com>
2019-08-13 20:47:38 +0300

    rest system
    no full implementation yet
    enemies status change on death is required

---
+++ Assets/Scripts/map management/PlayerRestCounter.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRestCounter : MonoBehaviour
+{
+    private void Awake()
+    {
+        if (PlayerPrefs.GetInt("PlayerRestCount", -1) == -1) PlayerPrefs.SetInt("PlayerRestCount", 1);
+    }
+    public void IncreaseRestCount()
+    {
+        PlayerPrefs.SetInt("PlayerRestCount", PlayerPrefs.GetInt("PlayerRestCount", 0)+1 );
+    }
+}
---
+++ Assets/Scripts/map management/RoomObject.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RoomObject : MonoBehaviour
+{
+    public string objectName;
+    public virtual void Awake()
+    {
+        GameObject.FindObjectOfType<Room>().onResetRoom += new Room.OnResetRoom(OnRoomReset);
+        objectName =SceneManager.GetActiveScene().name+"_"+gameObject.name;
+    }
+    public virtual void OnRoomReset()
+    {
+
+    }
+}
